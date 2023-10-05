4a6dc5517c892c9696255b09f847f4b90e900fc4
Ekaterina <b-ka95@mail.ru>
2023-10-06 01:19:08 +0300

    Lesson 4 exercise 5. Version 2

--- src/les4ex5.java
+++ src/les4ex5.java
@@ -11,11 +11,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.URL;
-
 public class les4ex5 {
-
     private AppiumDriver driver;
-
     @Before
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -31,13 +28,10 @@ public class les4ex5 {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
     }
-
     @After
     public void tearDown() {
         driver.quit();
     }
-
-
     @Test
     public void testSwipeArticle()
     {
@@ -54,10 +48,12 @@ public class les4ex5 {
                 5
         );
 
+        String search_phrase = "Appium";
+
         waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Appium",
-                "Cannot find search Appium",
+                search_phrase,
+                "Cannot find search "+search_phrase,
                 5
         );
 
@@ -78,12 +74,6 @@ public class les4ex5 {
                 "Cannot find button 'Add to list'",
                 5
         );
-
-//        waitForElementAndClick(
-//                By.xpath("//android.widget.TextView[@text='Create new']"),
-//                "Cannot find button Create a new list",
-//                5
-//        );
 
         String name_folder = "My first folder";
 
@@ -109,7 +99,7 @@ public class les4ex5 {
         waitForElementAndClick(
                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appius Claudius Caecus']"),
                 "Cannot find 'Appius Claudius Caecus' article in search",
-                5
+                10
         );
 
         waitForElementAndClick(
@@ -131,56 +121,47 @@ public class les4ex5 {
         );
 
         waitForElementAndClick(
-                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
-                "Cannot find Navigate arrow",
-                5
-        );
-        waitForElementAndClick(
-                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
-                "Cannot find Navigate arrow",
-                5
-        );
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/nav_tab_reading_lists"),
-                "Cannot find Saved",
-                5
-        );
-        waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/item_title'][@text='"+name_folder+"']"),
-                "Cannot find Folder",
+                By.xpath("//*[@resource-id='org.wikipedia:id/snackbar_action'][@text='View list']"),
+                "Cannot click on View list",
                 5
         );
 
+        String nameArticleOne = "Appium";
+
         swipeElementToLeft(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
-                "Cannot find Appium article in saved folder"
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + nameArticleOne + "']"),
+                "Cannot find " + nameArticleOne + " article in saved folder"
         );
 
         waitForElementNotPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
-                "Cannot delete Appium article",
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + nameArticleOne + "']"),
+                "Cannot delete " + nameArticleOne + " article",
                 5
         );
 
+        String nameArticleTwo = "Appius Claudius Caecus";
+
         WebElement title_element_in_folder = waitForElementPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appius Claudius Caecus']"),
-                "Cannot find 'Appius Claudius Caecus' article in folder",
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + nameArticleTwo + "']"),
+                "Cannot find " + nameArticleTwo + " article in folder",
                 5
         );
+
         String article_title_in_folder = title_element_in_folder.getAttribute("text");
 
         waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appius Claudius Caecus']"),
-                "Cannot click on 'Appius Claudius Caecus' article",
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + nameArticleTwo + "']"),
+                "Cannot click on " + nameArticleTwo + " article",
                 5
         );
 
         WebElement title_element_in_article = waitForElementPresent(
-                By.xpath("//android.view.View[@content-desc='Appius Claudius Caecus']"),
-                "Cannot find title 'Appius Claudius Caecus' in article",
+                By.xpath("//*[@resource-id='pcs-edit-section-title-description']/preceding-sibling::android.view.View"),
+                "Cannot find title " + nameArticleTwo + " in article",
                 5
         );
-        String article_title_in_article = title_element_in_article.getAttribute("text");
+
+        String article_title_in_article = title_element_in_article.getAttribute("contentDescription");
 
         Assert.assertEquals(
                 "We see unexpected title",
@@ -195,7 +176,7 @@ public class les4ex5 {
                 ExpectedConditions.presenceOfElementLocated(by)
         );
     }
-    private WebElement waitForElementPresent(By by, String error_message) {
+    private WebElement waitForElementPresent(By by,String error_message) {
         return waitForElementPresent(by,error_message,5);
     }
     private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
@@ -229,13 +210,11 @@ public class les4ex5 {
         int upper_y = element.getLocation().getY();
         int lower_y = upper_y + element.getSize().getHeight();
         int middle_y = (upper_y+lower_y) / 2;
-        TouchAction action = new TouchAction(driver);
-        action
-                .press(right_x,middle_y)
-                .waitAction(100)
-                .moveTo(left_x,middle_y)
+        new TouchAction(driver)
+                .press(right_x-10, middle_y)
+                .waitAction(300)
+                .moveTo(left_x +10, middle_y)
                 .release()
                 .perform();
     }
 }
-
