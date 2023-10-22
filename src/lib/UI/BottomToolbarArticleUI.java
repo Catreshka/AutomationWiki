93841428a42b3f0ae83dff8b4cfe6e889e7e77a2
Ekaterina <b-ka95@mail.ru>
2023-10-23 00:17:38 +0300

    Lesson 6 exercise 10

--- src/lib/UI/BottomToolbarArticleUI.java
+++ src/lib/UI/BottomToolbarArticleUI.java
@@ -1,16 +1,15 @@
 package lib.UI;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
 public class BottomToolbarArticleUI extends MainPageObject {
 
     private static final String
-            BUTTON_PAGE_SAVE = "org.wikipedia:id/page_save",
-            BUTTON_ADD_TO_LIST = "//*[@resource-id='org.wikipedia:id/snackbar_action'][@text='Add to list']",
-            INPUT_NAME_OF_FOLDER = "//*[contains(@text,'Name of this list')]",
-            BUTTON_OK = "//*[@resource-id='android:id/button1'][@text='OK']",
-            NAME_OF_EXIST_LIST_TPL = "//*[@resource-id='org.wikipedia:id/item_title'][@text='{SUBSTRING_NAME_OF_LIST}']",
-            BUTTON_VIEW_LIST = "//*[@resource-id='org.wikipedia:id/snackbar_action'][@text='View list']";
+            BUTTON_PAGE_SAVE = "id:org.wikipedia:id/page_save",
+            BUTTON_ADD_TO_LIST = "xpath://*[@resource-id='org.wikipedia:id/snackbar_action'][@text='Add to list']",
+            INPUT_NAME_OF_FOLDER = "xpath://*[contains(@text,'Name of this list')]",
+            BUTTON_OK = "xpath://*[@resource-id='android:id/button1'][@text='OK']",
+            NAME_OF_EXIST_LIST_TPL = "xpath://*[@resource-id='org.wikipedia:id/item_title'][@text='{SUBSTRING_NAME_OF_LIST}']",
+            BUTTON_VIEW_LIST = "xpath://*[@resource-id='org.wikipedia:id/snackbar_action'][@text='View list']";
 
     public BottomToolbarArticleUI(AppiumDriver driver)
     {
@@ -25,25 +24,24 @@ public class BottomToolbarArticleUI extends MainPageObject {
     /*TEMPLATES METHODS */
     public void addToList ()
     {
-        this.waitForElementAndClick(By.id(BUTTON_PAGE_SAVE), "Cannot find Save button", 5);
-        this.waitForElementAndClick(By.xpath(BUTTON_ADD_TO_LIST), "Cannot find button 'Add to list'", 5);
+        this.waitForElementAndClick(BUTTON_PAGE_SAVE, "Cannot find Save button", 5);
+        this.waitForElementAndClick(BUTTON_ADD_TO_LIST, "Cannot find button 'Add to list'", 5);
     }
 
     public void createMyList(String name_of_folder)
     {
-        this.waitForElementAndSendKeys(By.xpath(INPUT_NAME_OF_FOLDER), name_of_folder, "Cannot find input for write name of this list", 5);
-        this.waitForElementAndClick(By.xpath(BUTTON_OK), "Cannot find button 'OK'", 5);
+        this.waitForElementAndSendKeys(INPUT_NAME_OF_FOLDER, name_of_folder, "Cannot find input for write name of this list", 5);
+        this.waitForElementAndClick(BUTTON_OK, "Cannot find button 'OK'", 5);
 
     }
     public void addToExistList(String substring)
     {
         String name_of_saves_list = getNameOfList(substring);
-        this.waitForElementAndClick(By.xpath(name_of_saves_list), "Cannot find folder " + substring, 5);
+        this.waitForElementAndClick(name_of_saves_list, "Cannot find folder " + substring, 5);
     }
 
     public void viewExistListAfterSave()
     {
-        this.waitForElementAndClick(By.xpath(BUTTON_VIEW_LIST), "Cannot click on View list", 5);
+        this.waitForElementAndClick(BUTTON_VIEW_LIST, "Cannot click on View list", 5);
     }
-
 }
--- src/lib/UI/MainPageObject.java
+++ src/lib/UI/MainPageObject.java
@@ -8,8 +8,11 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import java.util.List;
+import java.time.Duration;
+import java.util.regex.Pattern;
 
 public class MainPageObject {
     protected AppiumDriver driver;
@@ -18,62 +21,66 @@ public class MainPageObject {
         this.driver = driver;
     }
 
-    public WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+    public WebElement waitForElementPresent(String locator, String error_message, long timeoutInSeconds) {
+        By by = this.getLocatorByString(locator);
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
         return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
         );
     }
-    public WebElement waitForElementPresent(By by, String error_message) {
-        return waitForElementPresent(by,error_message,5);
+    public WebElement waitForElementPresent(String locator, String error_message) {
+        return waitForElementPresent(locator,error_message,5);
     }
-    public WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
+    public WebElement waitForElementAndClick(String locator, String error_message, long timeoutInSeconds)
     {
-        WebElement element = waitForElementPresent(by,error_message,timeoutInSeconds);
+        WebElement element = waitForElementPresent(locator,error_message,timeoutInSeconds);
         element.click();
         return element;
     }
-    public WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
+    public WebElement waitForElementAndSendKeys(String locator, String value, String error_message, long timeoutInSeconds)
     {
-        WebElement element = waitForElementPresent(by,error_message,timeoutInSeconds);
+        WebElement element = waitForElementPresent(locator,error_message,timeoutInSeconds);
         element.sendKeys(value);
         return element;
     }
-    public boolean waitForElementNotPresent(By by, String error_message, long timeoutSeconds)
+    public boolean waitForElementNotPresent(String locator, String error_message, long timeoutSeconds)
     {
+        By by = this.getLocatorByString(locator);
         WebDriverWait wait = new WebDriverWait(driver,timeoutSeconds);
         wait.withMessage(error_message + "\n");
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
     }
-    public void assertElementHasText(By by, String expected_result, String error_message, String assert_error)
+    public void assertElementHasText(String locator, String expected_result, String error_message, String assert_error)
     {
-        WebElement element = waitForElementPresent(by,error_message);
+        WebElement element = waitForElementPresent(locator,error_message);
         String elementText = element.getAttribute("text");
         Assert.assertEquals(error_message,expected_result,elementText);
     }
-    public WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
+    public WebElement waitForElementAndClear(String locator, String error_message, long timeoutInSeconds)
     {
-        WebElement element = waitForElementPresent(by,error_message,timeoutInSeconds);
+        WebElement element = waitForElementPresent(locator,error_message,timeoutInSeconds);
         element.clear();
         return element;
     }
 
-    public void waitForWordInElement(By externalBy, By internalBy, String error_message, String value) {
+    public void waitForWordInElement(String externalLocator, String internalLocator, String error_message, String value) {
         driver.hideKeyboard();
-        waitForElementPresent(externalBy, error_message);
-        WebElement element= driver.findElement(externalBy);
-        List<WebElement> titles= element.findElements(internalBy);
+        By by_external = this.getLocatorByString(externalLocator);
+        waitForElementPresent(externalLocator, error_message);
+        WebElement element= driver.findElement(by_external);
+        By by_internal = this.getLocatorByString(internalLocator);
+        List<WebElement> titles= element.findElements(by_internal);
         boolean result = titles.stream().allMatch(selectText -> selectText.getText().contains(value));
         Assert.assertTrue(error_message, result);
     }
 
-    public void swipeElementToLeft(By by, String error_message)
+    public void swipeElementToLeft(String locator, String error_message)
     {
         WebElement element = waitForElementPresent(
-                by,
+                locator,
                 error_message,
                 10);
         int left_x = element.getLocation().getX();
@@ -82,15 +89,16 @@ public class MainPageObject {
         int lower_y = upper_y + element.getSize().getHeight();
         int middle_y = (upper_y+lower_y) / 2;
         new TouchAction(driver)
-                .press(right_x-10, middle_y)
-                .waitAction(300)
-                .moveTo(left_x +10, middle_y)
+                .press(PointOption.point(right_x-10, middle_y))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(300)))
+                .moveTo(PointOption.point(left_x +10, middle_y))
                 .release()
                 .perform();
     }
 
-    public void assertElementPresent (By by, String error_message)
+    public void assertElementPresent (String locator, String error_message)
     {
+        By by = this.getLocatorByString(locator);
         try {
             driver.findElement(by);
         } catch (NoSuchElementException e) {
@@ -98,9 +106,25 @@ public class MainPageObject {
         }
     }
 
-    public  int getAmountOfElements(By by)
+    public  int getAmountOfElements(String locator)
     {
+        By by = this.getLocatorByString(locator);
         List elements = driver.findElements(by);
         return elements.size();
+    }
+
+    private By getLocatorByString(String locator_with_type)
+    {
+        String[] exploded_locator = locator_with_type.split(Pattern.quote(":"),2);
+        String by_type = exploded_locator[0];
+        String locator = exploded_locator[1];
+
+        if (by_type.equals("xpath")) {
+            return By.xpath(locator);
+        } else if (by_type.equals("id")) {
+            return By.id(locator);
+        } else {
+            throw new IllegalArgumentException("Cannot get type of locator. Locator " + locator);
+        }
     }
 }
